0456de4f16ad3500cfeeacf6b936173c1495e308
kokand1 <ziz.uz3616524@gmail.com>
2026-02-21 15:46:16 +0500

    yangi file qo'shildi

---
+++ flutter_application_1/lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ikki extends StatefulWidget {
+  const ikki({super.key});
+
+  @override
+  State<ikki> createState() => _ikkiState();
+}
+
+class _ikkiState extends State<ikki> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
